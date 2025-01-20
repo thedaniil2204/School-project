c1d39e2eff9b01900f1cc1a8465785ec59aaf852
Daniil <ddandreev2008@gmail.com>
2025-01-20 21:55:39 +0300

    оброботка сообщения
    comit 1

--- commands.py
+++ commands.py
@@ -1,6 +1,9 @@
 import telebot
 from dotenv import load_dotenv
 import os
+
+from pyexpat.errors import messages
+
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
@@ -13,6 +16,16 @@ def start (message):
 @bot.message_handler(commands=["help"])
 def help (message):
     bot.send_message(message.chat.id, "Для того чтобы начать работу с ботом, вам необходио добавить его в чат в качестве администратора")
+
+
+@bot.message_handler()
+def text(message):
+    if message.text.lower() == "начнем рассчет":
+        bot.reply_to(message, "Напишите информацию о чеке в формате: cумма в рублях, имя пользователя совершившего покупу, способ разделения долга. Данные должны быть записаны в строчку без запятых через пробел")
+
+
+# Обработчик текстовых сообщений в процессе
+
 
 
 bot.polling(non_stop= True)
