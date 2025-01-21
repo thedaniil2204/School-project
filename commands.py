846084120e9cdfb02391da0b5c66bc7228f1452e
Daniil <ddandreev2008@gmail.com>
2025-01-21 23:14:52 +0300

    перепесь кода
    переписан код с помощью gpt, добавлено редактирование списка

--- commands.py
+++ commands.py
@@ -1,43 +1,103 @@
 import telebot
 from dotenv import load_dotenv
 import os
-
-from pyexpat.errors import messages
+from telebot.types import Message
 
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 bot = telebot.TeleBot(token=TELEGRAM_TOKEN)
 
+# Словарь для хранения данных о чеках
+check_data = {
+    "message_id": None,
+    "debtors": {}
+}
+
 @bot.message_handler(commands=["start"])
-def start (message):
+def start(message):
     bot.send_message(message.chat.id, f"Привет {message.from_user.first_name}")
 
 @bot.message_handler(commands=["help"])
-def help (message):
-    bot.send_message(message.chat.id, "Для того чтобы начать работу с ботом, вам необходио добавить его в чат в качестве администратора")
+def help(message):
+    bot.send_message(
+        message.chat.id,
+        "Для того чтобы начать работу с ботом, вам необходимо добавить его в чат в качестве администратора."
+    )
 
+user_list = []
+
+@bot.message_handler(commands=['add_users'])
+def add_users_handler(message: Message):
+    global user_list
+    user_list = message.text.split()[1:]
+    if user_list:
+        response = "Добавлены следующие пользователи:\n" + "\n".join(user_list)
+        bot.reply_to(message, response)
+    else:
+        bot.reply_to(message, "Вы не указали участников.")
 
 @bot.message_handler()
 def text(message):
     if message.text.lower() == "начнем":
-        bot.reply_to(message, "Напишите информацию о чеке в формате: cумма в рублях, имя пользователя совершившего покупу, список должников(через '_'). Данные должны быть записаны в строчку без запятых через пробел")
+        bot.reply_to(
+            message,
+            "Напишите информацию о чеке в формате:\n"
+            "сумма в рублях, имя пользователя, совершившего покупку.\n"
+            "Данные должны быть записаны в строчку без запятых через пробел."
+        )
         bot.register_next_step_handler(message, bill)
+    elif "оплатил" in message.text.lower():
+        handle_payment(message)
+
+sume = ""
 
 def bill(message):
-    bill1 = message.text.split()
-    bot.reply_to(message, f"получены данные {bill1}")
-    sume = bill1[0]
-    name = bill1[1]
-    spisok = bill1[2].split("_")
-    spisok_message = ", ".join(spisok)
-    bot.send_message(message.chat.id, f"Чек:\n Общаяя сумма: {sume}\n Имя покупающего: {name}\n Должники: {spisok_message} " )
+    global check_data
+    global sume
+    bill_data = message.text.split()
+    try:
+        sume = int(bill_data[0])
+    except ValueError:
+        bot.reply_to(message, "сумма должна быть записана числом")
 
-# нужно привести в порядок сообщения, добавить параметр описание и/или нозвание чека.
-# добавить редоктирование списка ботом
-# добавить обработку ошибок и исключений
+    name = bill_data[1]
+    spisok_message = []
+    check_data["debtors"] = {}
 
+    for user in user_list:
+        debt = round(int(sume) / len(user_list), 2)
+        spisok_message.append(f"{user} должен - {debt}")
+        check_data["debtors"][user] = f"должен - {debt}"
 
+    spisok_message = "\n".join(spisok_message)
+    sent_message = bot.send_message(
+        message.chat.id,
+        f"Чек:\n"
+        f"Общая сумма: {sume}\n"
+        f"Имя покупающего: {name}\n"
+        f"Должники:\n{spisok_message}"
+    )
+    check_data["message_id"] = sent_message.message_id
+    bot.pin_chat_message(chat_id=message.chat.id, message_id=sent_message.message_id)
+
+def handle_payment(message):
+    global check_data
+    username = message.text.split()[0]  # Получаем username из сообщения
+    if username in check_data["debtors"]:
+        check_data["debtors"][username] = "оплатил"
+        updated_message = f"Чек:\nОбшая сумма: {sume}\nДолжники:\n"
+        for user, status in check_data["debtors"].items():
+            updated_message += f"{user} {status}\n"
+        bot.edit_message_text(
+            chat_id=message.chat.id,
+            message_id=check_data["message_id"],
+            text=updated_message.strip()
+        )
+    else:
+        bot.reply_to(message, "Этот пользователь не найден в списке должников.")
+
+# Запуск бота
 if __name__ == '__main__':
     print('Бот запущен!')
     bot.infinity_polling()
